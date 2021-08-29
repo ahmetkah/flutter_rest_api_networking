b8b6f3b08e535e3262dfa22415ead2c89f4ed525
Ahmet Kahrimanoğlu <ahmet.kahrimanoglu@gmail.com>
2021-08-29 20:25:21 +0300

    main.dart ve app.dar'a yorum satırları eklendi

--- lib/app.dart
+++ lib/app.dart
@@ -7,10 +7,26 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    /// [MaterialApp] widgeti:
+    /// Uygulamayı en dıştan saran ve ana kapsayıcı widget'dır.
+    /// Uygulama ana arayüzünün temeli burada atılır.
     return MaterialApp(
+      /// [title] parametresi: "Uygulama Adı/Başlığı"
       title: 'Networking',
+
+      /// [debugShowCheckedModeBanner] parametresi:
+      /// Sağ üstte Debug adında bir etiket yer alır.
+      /// Bu etiketi kaldırmak (false) / göstermek (true) için kullanılır.
       debugShowCheckedModeBanner: false,
+
+      /// [theme] parametresi:
+      /// Uygulamanın genel teması
       theme: ThemeData.dark(),
+
+      /// [onGenerateRoute] parametresi:
+      /// Bu parametreye routes\routes.dart sayfasında uygulama boyunca
+      /// kullanacağımız adlandırılmış sayfa rotaları için
+      /// oluşturduğumuz [route()] isimli metodu verdik.
       onGenerateRoute: Routes.instance!.route,
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,20 @@ import 'package:flutter/material.dart';
 
 import 'app.dart';
 
+/// [main()] metodu:
+/// Uygulama çalıştırılacağı zaman Flutter ilk olarak bu metodu bulur
+/// ve uygulama buradan çalışır.
+/// Uygulama da tek bir tane main() metodu vardır.
+/// [void] ön eki:
+/// main() metodunun herhangi bir değer döndürmediğini ifade eder.
 void main() {
+  /// [runApp()] metodu:
+  /// Uygulamanın kök widgetini çalıştırırarak ekrana görüntü gelmesini sağlar.
   runApp(
+    /// [App] widgeti:
+    /// Widget ağacımızın kökü olan ve bir [MaterialApp] döndürecek olan widgettir.
+    /// Okunurluk açısından bu [App] widgetini app.dart isimli
+    /// ayrı bir dart dosyasında oluşturduk.
     const App(),
   );
 }
