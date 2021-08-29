4a02bc9c60972aadf0b43e6dbdb4769b4a725568
Ahmet Kahrimanoğlu <ahmet.kahrimanoglu@gmail.com>
2021-08-29 20:51:30 +0300

    post.dart'a yorum satırlari eklendi

--- lib/models/post.dart
+++ lib/models/post.dart
@@ -1,4 +1,21 @@
+/// [1. Adım]: Model Sınıfının Oluşturulması
+/// JSONPlaceHolder'un aşağıdaki endpoint'i kullanın.
+/// Posts: https://jsonplaceholder.typicode.com/posts
+/// Yukarıdaki bağlantı adresini kopyalayarak bağlantıya gidin.
+/// Açılan sayfanın içindeki tüm JSON verilerini kopyalayın.
+/// Bu ham verileri Dart nesnelerine dönüştürmek için Quicktype
+/// (https://app.quicktype.io) isimli online aracı kullanın.
+/// Quicktype'ın sitesine gidin, kopyalanan verileri yapıştırın,
+/// Modele Post ismini verin, "Make all properties final" seçeneğini aktifleştirin
+/// ve dönüştürülecek dilin Dart olduğunu belirtin.
+/// Dönüştürülen kodların tamamını Copy Code düğmesiyle kopyalayın.
+/// lib>models klasörü altında model ile aynı isimde olacak şekilde
+/// post.dart isimli bir dart dosyası oluşturun.
+/// Not 1: Quicktype'ın otomatik oluşturduğu postFromJson() ve photoToJson()
+/// isimli metotları silin. Ayrıca import ile başlayan satırıda silin.
+/// Not 2: Burada [1] ve [2] ile belirtilen düzeltmeleri elle yapın.
 class Post {
+  /// [1] Constructor (Kurucu)'ı [const] yapın.
   const Post({
     this.userId,
     this.id,
@@ -6,6 +23,8 @@ class Post {
     this.body,
   });
 
+  /// [2] REST API'dan beklenen alanların null gelebileceğini
+  /// belirtmek için başlarına soru işareti [?] ekleyin.
   final int? userId;
   final int? id;
   final String? title;
